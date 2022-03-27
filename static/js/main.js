1b0661f2532020a55064fe3621e226d2747ff8fa
ssfdust <ssfdust@gmail.com>
2022-03-27 21:14:48 +0800

    Initilize

---
+++ static/js/main.js
@@ -0,0 +1,36 @@
+document.ready = function (callback) {
+    ///兼容FF,Google
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', function () {
+            document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+            callback();
+        }, false)
+    }
+    //兼容IE
+    else if (document.attachEvent) {
+        document.attachEvent('onreadystatechange', function () {
+            if (document.readyState == "complete") {
+                document.detachEvent("onreadystatechange", arguments.callee);
+                callback();
+            }
+        })
+    }
+    else if (document.lastChild == document.body) {
+        callback();
+    }
+}
+
+function get_one_say() {
+    axios({
+        method: "get",
+        url: "/api/huayen/onesay"
+    })
+        .then(function ({data: resp}) {
+            let onesay = document.getElementById("onesay");
+            onesay.textContent = resp.data.onesay;
+        });
+}
+
+document.ready(function () {
+    get_one_say();
+})
